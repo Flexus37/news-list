b26610bd1d51b0920ad802e7c1bdf2bc5f3420d3
Alexey Dudin <aleshadudin53@gmail.com>
2024-06-26 13:20:50 +0300

    refactor: update header styles and class names
    - Adjust styles to use rem units
    - Update class names for better readability

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,5 @@
-import RefreshIcon from '@/public/icons/refresh.svg'
-import SearchIcon from '@/public/icons/search.svg'
+import RefreshIcon from '@/public/icons/header/refresh.svg'
+import SearchIcon from '@/public/icons/header/search.svg'
 import styles from './header.module.scss'
 
 export function Header() {
@@ -9,15 +9,15 @@ export function Header() {
 			<div className={styles.item}>
 				<h1 className={styles.title}>Список новостей</h1>
 				<button className={styles.refresh}>
-					<RefreshIcon className={styles.refresh__icon} alt='Cбросить' />
+					<RefreshIcon className={styles.refreshIcon} alt='Cбросить' />
 				</button>
 			</div>
 
 			<div className={styles.item}>
 				<div className={styles.search__wrapper}>
 					<input className={styles.search} type='search' />
-					<button className={styles.search__btn}>
-						<SearchIcon className={styles.search__icon} alt='Найти' />
+					<button className={styles.searchBtn}>
+						<SearchIcon className={styles.searchIcon} alt='Найти' />
 					</button>
 				</div>
 
