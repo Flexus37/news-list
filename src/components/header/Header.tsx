74fc4667f8734c469cf76ddc26e4454a61abc8e0
Alexey Dudin <aleshadudin53@gmail.com>
2024-06-30 02:53:54 +0300

    feat(header): update Header component functionality and styling
    - Refactored Header component to include search filter functionality and data refresh
    - Added `use client` directive and React hooks for state management
    - Implemented `handleSearchChange` for updating search query parameter
    - Implemented `handleRefresh` for resetting filters and revalidating data
    - Updated styles in `header.module.scss` to include responsive design with @media queries

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,27 +1,73 @@
+'use client'
+
 import RefreshIcon from '@/public/icons/header/refresh.svg'
 import SearchIcon from '@/public/icons/header/search.svg'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+
+import { createQueryString } from '@/utils/queryString'
+import handleRevalidate from '@/utils/revalidateData'
+import { useEffect, useState } from 'react'
 import styles from './header.module.scss'
 
 export function Header() {
-	return (
-		<header className={styles.header}>
+	const [searchInputValue, setSearchInputValue] = useState<string>('');
 
-			<div className={styles.item}>
-				<h1 className={styles.title}>Список новостей</h1>
-				<button className={styles.refresh}>
-					<RefreshIcon className={styles.refreshIcon} alt='Cбросить' />
-				</button>
-			</div>
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
-			<div className={styles.item}>
-				<div className={styles.search__wrapper}>
-					<input className={styles.search} type='search' />
-					<button className={styles.searchBtn}>
-						<SearchIcon className={styles.searchIcon} alt='Найти' />
-					</button>
-				</div>
+	useEffect(() => {
+		setSearchInputValue(searchParams.get('search') ?? '');
+	}, [])
 
-			</div>
-		</header>
-	)
+  // Изменение фильтра поиска
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setSearchInputValue(e.target.value);
+    const searchTerm = e.target.value.trim().toLowerCase();
+
+    // Замена query params search=
+    router.replace(`/1/?${createQueryString(searchParams, 'search', searchTerm)}`);
+  }
+
+  // Ручная перезагрузка данных и сброс всех фильтров
+	const handleRefresh = async () => {
+		const newSearchParams = createQueryString(searchParams, 'filter', 'все');
+    const finalSearchParams = createQueryString(newSearchParams, 'search', '');
+		setSearchInputValue('');
+
+    router.replace(`${pathname}?${finalSearchParams}`);
+
+    // Ревалидация данных
+    await handleRevalidate();
+	}
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Список новостей</h1>
+        <button
+          onClick={() => handleRefresh()}
+          className={styles.refresh}
+        >
+          <RefreshIcon className={styles.refreshIcon} alt='Cбросить' />
+        </button>
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.searchWrapper}>
+          <input
+            className={styles.search}
+            type='search'
+						value={searchInputValue}
+						defaultValue={searchInputValue}
+            onChange={(e) => handleSearchChange(e)}
+
+          />
+          <button className={styles.searchBtn}>
+            <SearchIcon className={styles.searchIcon} alt='Найти' />
+          </button>
+        </div>
+      </div>
+    </header>
+  )
 }
