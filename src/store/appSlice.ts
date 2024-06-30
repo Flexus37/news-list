76fc078eb368fcc1dd757952501d771665f54194
Alexey Dudin <aleshadudin53@gmail.com>
2024-06-30 03:14:21 +0300

    refactor(store): remove unnecessary code and dependencies from appSlice
    - Удалены ненужные действия и редьюсеры
    - Оставлен только необходимый тип Filter для использования в других компонентах

--- src/store/appSlice.ts
+++ src/store/appSlice.ts
@@ -1,18 +1,13 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 export type ViewMode = 'list' | 'grid';
-export type Filter = 'Все' | 'Lenta.ru' | 'Mos.ru'
 
 interface AppState {
   viewMode: ViewMode;
-  filter: Filter;
-  searchTerm: string;
 }
 
 const initialState: AppState = {
   viewMode: 'list',
-  filter: 'Все',
-  searchTerm: '',
 };
 
 const appSlice = createSlice({
@@ -22,19 +17,11 @@ const appSlice = createSlice({
     setViewMode(state, action: PayloadAction<ViewMode>) {
       localStorage.setItem('viewMode', action.payload);
       state.viewMode = action.payload;
-    },
-    setFilter(state, action: PayloadAction<Filter>) {
-      state.filter = action.payload;
-    },
-    setSearchTerm(state, action: PayloadAction<string>) {
-      state.searchTerm = action.payload;
-    },
+    }
   },
 });
 
 export const {
 	setViewMode,
-	setFilter,
-	setSearchTerm
 } = appSlice.actions;
 export default appSlice.reducer;
